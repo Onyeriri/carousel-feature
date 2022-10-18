a0193bcbe28e95ab4c0ef278d2af610f7134c324
Onyeriri Victor <viktorchinonso@gmail.com>
2022-10-18 13:43:10 +0100

    Added mobile feature to the project

--- script/script.js
+++ script/script.js
@@ -4,6 +4,10 @@ const nextButton = document.querySelector(".carousel__button--right");
 const prevButton = document.querySelector(".carousel__button--left");
 const dotsNav = document.querySelector(".carousel__nav");
 const dots = Array.from(dotsNav.children);
+const currentSlide = track.querySelector(".current-slide");
+const prevSlide = currentSlide.previousElementSibling;
+
+disableLeftButton(prevSlide);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 // console.log(track);
@@ -14,14 +18,61 @@ const setSlidePosition = (slide, index) => {
 };
 slides.forEach(setSlidePosition);
 
+// function to move each image in the carousel slide
+const moveToSlide = (track, currentSlide, targetSlide) => {
+  track.style.transform = "translateX(-" + targetSlide.style.left + ")";
+  currentSlide.classList.remove("current-slide");
+  targetSlide.classList.add("current-slide");
+};
+
 // when I click right, move slides to the left
 nextButton.addEventListener("click", (e) => {
   const currentSlide = track.querySelector(".current-slide");
-  console.log(currentSlide);
+
   const nextSlide = currentSlide.nextElementSibling;
-  const amountToMove = nextSlide.style.left;
-  // move to the next slide
-  track.style.transform = "translateX(-" + amountToMove + ")";
-  currentSlide.classList.remove("current-slide");
-  nextSlide.classList.add("current-slide");
+
+  moveToSlide(track, currentSlide, nextSlide);
+  disableDirectionButton(nextSlide, ".carousel__button--right");
 });
+
+// when I click left, move slides to the left
+prevButton.addEventListener("click", (e) => {
+  const currentSlide = track.querySelector(".current-slide");
+  const prevSlide = currentSlide.previousElementSibling;
+
+  moveToSlide(track, currentSlide, prevSlide);
+  disableDirectionButton(prevSlide, ".carousel__button--left");
+});
+
+// when direction button disable when you get to the last image on the carousel
+const disableDirectionButton = (slide, className) => {
+  // console.log(slides.indexOf(slide));
+  // console.log(slides.indexOf(slides[slides.length - 1]));
+
+  if (slides.indexOf(slide) < slides.indexOf(slides[slides.length - 1])) {
+    document.querySelector(".carousel__button--right").disabled = false;
+    document.querySelector(".carousel__button--right").style.cursor = "pointer";
+
+    // console.log("am in");
+  }
+
+  if (slides.indexOf(slide) > 0) {
+    document.querySelector(".carousel__button--left").disabled = false;
+    document.querySelector(".carousel__button--left").style.cursor = "pointer";
+  }
+
+  if (
+    slides.indexOf(slides[slides.length - 1]) === slides.indexOf(slide) ||
+    slides.indexOf(slides[0]) === slides.indexOf(slide)
+  ) {
+    document.querySelector(`${className}`).disabled = true;
+    document.querySelector(`${className}`).style.cursor = "none";
+  }
+};
+
+function disableLeftButton(slide) {
+  if (slides.indexOf(slide) !== null) {
+    document.querySelector(".carousel__button--left").disabled = true;
+    document.querySelector(".carousel__button--left").style.cursor = "none";
+  }
+}
